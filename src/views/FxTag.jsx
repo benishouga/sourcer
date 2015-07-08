25d791fd9ad7eb03e43a2e7049f2d817e2f3937b
benishouga <benishouga@gmail.com>
2015-07-09 01:24:43 +0900

    fxの影も一旦削除

--- src/views/FxTag.jsx
+++ src/views/FxTag.jsx
@@ -6,7 +6,7 @@ var FxTag = React.createClass({
     var model = this.props.model;
     var frame = model.frame;
     return (
-      <g style={{filter: "url(#f1)"}}>
+      <g>
         <g transform={"translate(" + model.position.x + "," + model.position.y + ")"}>
           <circle r={frame / 2 + 6} cy="0" cx="0" fill={"rgba(255,255,255," + (255 * frame / model.length) + ")"} />
           <circle r={frame / 2 + 5} cy="0" cx="0" fill={"rgba(0,0,0," + (255 * frame / model.length) + ")"} />
