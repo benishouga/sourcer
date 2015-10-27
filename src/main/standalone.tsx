decb407bb53275da9f5f214c858675a9c438efe2
benishouga <benishouga@gmail.com>
2015-10-27 21:55:12 +0900

    サイズをHTMLから指定できるように修正

--- src/main/standalone.tsx
+++ src/main/standalone.tsx
@@ -30,10 +30,14 @@ class Standalone {
         this.frames.push(e.data.field);
       }
     });
+
+    var player1 = document.getElementById("player1") as HTMLTextAreaElement;
+    var player2 = document.getElementById("player2") as HTMLTextAreaElement;
+
     this.worker.postMessage({
       sources: [
-        { name: "player1", color: "#866", ai: $("#player1").val() },
-        { name: "player2", color: "#262", ai: $("#player2").val() }
+        { name: "player1", color: "#866", ai: player1.value },
+        { name: "player2", color: "#262", ai: player2.value }
       ]
     });
   }
@@ -41,7 +45,7 @@ class Standalone {
 
 var standalone = new Standalone();
 
-class ScreenTag extends React.Component<{ width: number; height: number }, { field?: FieldDump; playing?: boolean }> {
+class ScreenTag extends React.Component<{ width: number; height: number; scale: number }, { field?: FieldDump; playing?: boolean }> {
   constructor() {
     super();
     this.state = { field: null, playing: true };
@@ -62,8 +66,11 @@ class ScreenTag extends React.Component<{ width: number; height: number }, { fie
   }
 
   render() {
+    var scale = this.props.scale;
     var width = this.props.width;
     var height = this.props.height;
+    var scaledWidth = this.props.width / scale;
+    var scaledHeight = this.props.height / scale;
 
     if (standalone.endOfGame) {
       var onFrameChanged = (newFrame: number) => {
@@ -71,9 +78,11 @@ class ScreenTag extends React.Component<{ width: number; height: number }, { fie
       };
       return (
         <svg width={width} height={height} viewBox={(-width / 2) + " 0 " + width + " " + height}>
-          <FieldTag field={this.state.field} width={width} height={height} frameLength={standalone.frames.length}
-          playing={this.state.playing} onFrameChanged={onFrameChanged} onPlay={() => this.onPlay() } onPause={() => this.onPause() } onReload={() => this.onReload() } />
-        </svg>
+          <g transform={"scale(" + scale + ", " + scale + ")"}>
+            <FieldTag field={this.state.field} width={scaledWidth} height={scaledHeight} frameLength={standalone.frames.length}
+              playing={this.state.playing} onFrameChanged={onFrameChanged} onPlay={() => this.onPlay() } onPause={() => this.onPause() } onReload={() => this.onReload() } />
+            </g>
+          </svg>
       );
     } else {
       return (<p>Loading...</p>);
@@ -94,16 +103,16 @@ class ScreenTag extends React.Component<{ width: number; height: number }, { fie
     }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     requestAnimationFrame(() => this.tick());
   }
 }
 
-let screens = document.getElementsByClassName("sourcer");
+let screens = document.getElementsByClassName("sourcer-standalone");
 for (let i = 0; i < screens.length; i++) {
   let output = screens[i];
   var width = parseInt(output.getAttribute('data-width')) || 512;
   var height = parseInt(output.getAttribute('data-height')) || 384;
   var scale = parseInt(output.getAttribute('data-scale')) || 1.0;
-  React.render(<ScreenTag width={width} height={height} />, output);
+  React.render(<ScreenTag width={width} height={height} scale={scale} />, output);
 }
