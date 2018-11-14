fe5672f5473f20fc0e99bd5709c2a11ae97c516a
benishouga <benishouga@gmail.com>
2018-11-14 21:17:24 +0900

    Use hooks at AceEditor.tsx

--- src/main/client/components/parts/AceEditor.tsx
+++ src/main/client/components/parts/AceEditor.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import * as Ace from 'brace';
 import 'brace/mode/javascript';
 import 'brace/theme/chrome';
@@ -13,77 +12,71 @@ export interface AceEditorProps {
   onSave?: () => void;
 }
 
-export default class AceEditor extends React.Component<AceEditorProps, {}> {
-  private editor: Ace.Editor | null = null;
-  private silent: boolean = false;
+const STYLE = { fontSize: '14px !important', border: '1px solid lightgray' };
+const DEFAULT_PROPS = {
+  code: '//write your code here',
+  cursorStart: 1
+};
 
-  public static defaultProps = {
-    code: '//write your code here',
-    cursorStart: 1
-  };
+export default function AceEditor(arg: AceEditorProps) {
+  const props: AceEditorProps = { ...DEFAULT_PROPS, ...arg };
+  const root = React.useRef<HTMLDivElement>(null);
 
-  public componentDidMount() {
-    const refs = this.refs as any;
-    const node = ReactDOM.findDOMNode(refs.root) as HTMLElement;
-    node.addEventListener('keydown', this.onKeyDown);
-    this.editor = Ace.edit(node);
-    this.editor.$blockScrolling = Infinity;
-    this.editor.setTheme('ace/theme/chrome');
-    const session = this.editor.getSession();
-    session.setMode('ace/mode/javascript');
-    session.setTabSize(2);
-    this.editor.setShowPrintMargin(false);
-    this.editor.setFontSize('11');
-    this.editor.setOptions({ minLines: 42, maxLines: 42 });
-    this.editor.setValue(this.props.code || '', this.props.cursorStart);
-    this.editor.setReadOnly(this.props.readOnly);
-    this.editor.on('change', this.onChange.bind(this));
-    this.editor.commands.addCommand({
-      name: 'Save',
-      exec: (_editor: Ace.Editor) => {
-        if (this.props.onSave) {
-          this.props.onSave();
-        }
-      },
-      bindKey: { win: 'Ctrl-S', mac: 'Command-S' },
-      readOnly: true
-    });
-  }
-
-  public shouldComponentUpdate(_nextProps: AceEditorProps) {
-    return false;
-  }
-
-  public onChange() {
-    if (this.props.onChange && !this.silent && this.editor) {
-      this.props.onChange(this.editor.getValue());
+  function onChange(editor: Ace.Editor | null) {
+    if (props.onChange && editor) {
+      props.onChange(editor.getValue());
     }
   }
 
-  public componentWillUnmount() {
-    if (this.editor) {
-      this.editor.destroy();
-      this.editor = null;
-    }
-    const refs = this.refs as any;
-    const node = ReactDOM.findDOMNode(refs.root) as HTMLElement;
-    node.removeEventListener('keydown', this.onKeyDown);
-  }
-
-  public render() {
-    const style = { fontSize: '14px !important', border: '1px solid lightgray' };
-    return (
-      <div ref="root" style={style} className={this.props.className}>
-        {this.props.code}
-      </div>
-    );
-  }
-
-  private onKeyDown = (event: KeyboardEvent) => {
+  function onKeyDown(event: KeyboardEvent) {
     switch (event.keyCode) {
       case 32: // Space
         event.stopPropagation();
         break;
     }
-  };
+  }
+
+  React.useEffect(() => {
+    if (!root.current) {
+      return;
+    }
+    root.current.addEventListener('keydown', onKeyDown);
+    let editor: Ace.Editor | null = Ace.edit(root.current);
+    editor.$blockScrolling = Infinity;
+    editor.setTheme('ace/theme/chrome');
+    const session = editor.getSession();
+    session.setMode('ace/mode/javascript');
+    session.setTabSize(2);
+    editor.setShowPrintMargin(false);
+    editor.setFontSize('11');
+    editor.setOptions({ minLines: 42, maxLines: 42 });
+    editor.setValue(props.code || '', props.cursorStart);
+    editor.setReadOnly(props.readOnly);
+    editor.on('change', () => onChange(editor));
+    editor.commands.addCommand({
+      name: 'Save',
+      exec: (_editor: Ace.Editor) => {
+        if (props.onSave) {
+          props.onSave();
+        }
+      },
+      bindKey: { win: 'Ctrl-S', mac: 'Command-S' },
+      readOnly: true
+    });
+    return () => {
+      if (editor) {
+        editor.destroy();
+        editor = null;
+      }
+      if (root.current) {
+        root.current.removeEventListener('keydown', onKeyDown);
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={root} style={STYLE} className={props.className}>
+      {props.code}
+    </div>
+  );
 }
