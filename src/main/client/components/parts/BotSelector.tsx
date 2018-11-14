d49b0f592265287e6d5f2d1bf90a361dd353b369
benishouga <benishouga@gmail.com>
2018-11-14 19:24:35 +0900

    use hooks at BotSelector.tsx

--- src/main/client/components/parts/BotSelector.tsx
+++ src/main/client/components/parts/BotSelector.tsx
@@ -17,89 +17,66 @@ export interface BotSelectorProps {
   selected?: string;
 }
 
-export interface BotSelectorState {
-  selectedSource?: string;
-  isShowCode?: boolean;
-  selectButtonLabel?: string;
-}
+const LEFT_BUTTON_STYLE = {
+  borderTopLeftRadius: '2px',
+  borderBottomLeftRadius: '2px',
+  borderTopRightRadius: '0',
+  borderBottomRightRadius: '0'
+};
 
-export default class BotSelector extends React.Component<BotSelectorProps, BotSelectorState> {
-  constructor(props: BotSelectorProps) {
-    super(props);
-    const resource = strings();
-    this.state = {
-      selectedSource: props.selected,
-      isShowCode: false,
-      selectButtonLabel: resource.selectEnemy
-    };
-  }
+const RIGHT_BUTTON_STYLE = {
+  borderTopLeftRadius: '0',
+  borderBottomLeftRadius: '0',
+  borderTopRightRadius: '2px',
+  borderBottomRightRadius: '2px'
+};
 
-  public onSelect(source: string, label: string) {
-    this.setState({
-      selectedSource: source,
-      selectButtonLabel: label
-    });
-    if (this.props.onSelect) {
-      this.props.onSelect(source);
+export default function BotSelector(props: BotSelectorProps) {
+  const resource = strings();
+  const [selectedSource, setSelectedSource] = React.useState('');
+  const [selectButtonLabel, setSelectButtonLabel] = React.useState(resource.selectEnemy);
+  const [isShowCode, setIsShowCode] = React.useState(false);
+
+  function onSelect(source: string, label: string) {
+    setSelectButtonLabel(label);
+    setSelectedSource(source);
+    if (props.onSelect) {
+      props.onSelect(source);
     }
   }
 
-  public toggleCodeViewer() {
-    this.setState({
-      isShowCode: !this.state.isShowCode
-    });
+  function toggleCodeViewer() {
+    setIsShowCode(!isShowCode);
   }
 
-  public render() {
-    const resource = strings();
-
-    const leftButtonStyle = {
-      borderTopLeftRadius: '2px',
-      borderBottomLeftRadius: '2px',
-      borderTopRightRadius: '0',
-      borderBottomRightRadius: '0'
-    };
-
-    const rightButtonStyle = {
-      borderTopLeftRadius: '0',
-      borderBottomLeftRadius: '0',
-      borderTopRightRadius: '2px',
-      borderBottomRightRadius: '2px'
-    };
-
-    return (
-      <div style={{ marginBottom: '8px' }}>
-        <div style={{ position: 'relative', textAlign: 'right' }}>
-          <Button raised ripple colored id="enemy-select-menu" style={leftButtonStyle}>
-            <Icon name="android" /> {this.state.selectButtonLabel}
+  return (
+    <div style={{ marginBottom: '8px' }}>
+      <div style={{ position: 'relative', textAlign: 'right' }}>
+        <Button raised ripple colored id="enemy-select-menu" style={LEFT_BUTTON_STYLE}>
+          <Icon name="android" /> {selectButtonLabel}
+        </Button>
+        <Menu target="enemy-select-menu" align="right">
+          <MenuItem onClick={() => onSelect(fiddle, resource.fiddle)}>{resource.fiddle}</MenuItem>
+          <MenuItem onClick={() => onSelect(fewAttack, resource.fewAttack)}>{resource.fewAttack}</MenuItem>
+          <MenuItem onClick={() => onSelect(fewMissile, resource.fewMissile)}>{resource.fewMissile}</MenuItem>
+          <MenuItem onClick={() => onSelect(escape, resource.escape)}>{resource.escape}</MenuItem>
+          <MenuItem onClick={() => onSelect(fixedBattery, resource.attacksFromLow)}>{resource.attacksFromLow}</MenuItem>
+          <MenuItem onClick={() => onSelect(standard, resource.standard)}>{resource.standard}</MenuItem>
+        </Menu>
+        <Tooltip label={resource.viewCode} position="bottom">
+          <Button raised ripple colored style={RIGHT_BUTTON_STYLE} onClick={() => toggleCodeViewer()}>
+            <Icon name={isShowCode ? 'visibility_off' : 'visibility'} />
           </Button>
-          <Menu target="enemy-select-menu" align="right">
-            <MenuItem onClick={this.onSelect.bind(this, fiddle, resource.fiddle)}>{resource.fiddle}</MenuItem>
-            <MenuItem onClick={this.onSelect.bind(this, fewAttack, resource.fewAttack)}>{resource.fewAttack}</MenuItem>
-            <MenuItem onClick={this.onSelect.bind(this, fewMissile, resource.fewMissile)}>
-              {resource.fewMissile}
-            </MenuItem>
-            <MenuItem onClick={this.onSelect.bind(this, escape, resource.escape)}>{resource.escape}</MenuItem>
-            <MenuItem onClick={this.onSelect.bind(this, fixedBattery, resource.attacksFromLow)}>
-              {resource.attacksFromLow}
-            </MenuItem>
-            <MenuItem onClick={this.onSelect.bind(this, standard, resource.standard)}>{resource.standard}</MenuItem>
-          </Menu>
-          <Tooltip label={resource.viewCode} position="bottom">
-            <Button raised ripple colored style={rightButtonStyle} onClick={this.toggleCodeViewer.bind(this)}>
-              <Icon name={this.state.isShowCode ? 'visibility_off' : 'visibility'} />
-            </Button>
-          </Tooltip>
-        </div>
-        <div style={{ display: this.state.isShowCode ? 'block' : 'none' }}>
-          <AceEditor
-            key={this.state.selectedSource /* HACK */}
-            code={this.state.selectedSource || ''}
-            className="mdl-shadow--4dp"
-            readOnly={true}
-          />
-        </div>
+        </Tooltip>
       </div>
-    );
-  }
+      <div style={{ display: isShowCode ? 'block' : 'none' }}>
+        <AceEditor
+          key={selectedSource /* HACK */}
+          code={selectedSource || ''}
+          className="mdl-shadow--4dp"
+          readOnly={true}
+        />
+      </div>
+    </div>
+  );
 }
