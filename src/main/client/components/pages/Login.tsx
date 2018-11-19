564a20e59931eb8afc3e59e282ec29b64ff4fd61
benishouga <benishouga@gmail.com>
2018-11-19 20:03:20 +0900

    Use hooks at Login.tsx

--- src/main/client/components/pages/Login.tsx
+++ src/main/client/components/pages/Login.tsx
@@ -7,68 +7,62 @@ import { strings } from '../resources/Strings';
 import Auth from '../../service/Auth';
 import ComponentExplorer from '../../utils/ComponentExplorer';
 
-export interface LoginState {
-  error?: boolean;
-  redirectToReferrer: boolean;
-  admin: boolean;
-}
+export default function Login(props: RouteComponentProps<{}>) {
+  const [error, setError] = React.useState<boolean>(false);
+  const [redirectToReferrer, setRedirectToReferrer] = React.useState<boolean>(false);
+  const [admin, setAdmin] = React.useState<boolean>(false);
 
-export default class Login extends React.Component<RouteComponentProps<{}>, LoginState> {
-  constructor(props: RouteComponentProps<{}>) {
-    super(props);
-    this.state = { error: false, redirectToReferrer: false, admin: false };
-  }
+  const accountRef = React.useRef<Textfield>(null);
+  const passwordRef = React.useRef<Textfield>(null);
 
-  private async handleSubmit(event: React.FormEvent<{}>) {
+  const { from } = props.location.state || { from: { pathname: '/' } };
+
+  async function handleSubmit(event: React.FormEvent<{}>) {
     event.preventDefault();
-
-    const account = ComponentExplorer.extractInputValue(this.refs.account);
-    const password = ComponentExplorer.extractInputValue(this.refs.password);
+    const account = ComponentExplorer.extractInputValue(accountRef.current);
+    const password = ComponentExplorer.extractInputValue(passwordRef.current);
 
     const loggedIn = await Auth.login({ account, password });
 
     if (!loggedIn.authenticated) {
-      return this.setState({ error: true });
+      return setError(true);
     }
-    this.setState({ redirectToReferrer: true, admin: loggedIn.admin });
+    setAdmin(loggedIn.admin);
+    setRedirectToReferrer(true);
   }
 
-  public render() {
-    const { from } = this.props.location.state || { from: { pathname: '/' } };
-    const { redirectToReferrer, admin } = this.state;
-    if (redirectToReferrer) {
-      return <Redirect to={admin ? '/official' : from} />;
-    }
-
-    const resource = strings();
-    return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <Card shadow={0} style={{ margin: 'auto' }}>
-          <CardTitle expand style={{ alignItems: 'flex-start' }}>
-            {resource.loginTitle}
-          </CardTitle>
-          <CardText>
-            <Textfield label={resource.fieldLabelAccount} floatingLabel ref="account" />
-            <Textfield label={resource.fieldLabelPassword} floatingLabel ref="password" type="password" />
-            {this.state.error && <p>{resource.badRequest}</p>}
-          </CardText>
-          <CardActions
-            border
-            style={{
-              borderColor: 'rgba(255, 255, 255, 0.2)',
-              display: 'flex',
-              boxSizing: 'border-box',
-              alignItems: 'center'
-            }}
-          >
-            <Button raised colored ripple onClick={this.handleSubmit.bind(this)}>
-              {resource.login}
-            </Button>
-            <Spacer />
-            <Icon name="account_box" />
-          </CardActions>
-        </Card>
-      </form>
-    );
+  if (redirectToReferrer) {
+    return <Redirect to={admin ? '/official' : from} />;
   }
+
+  const resource = strings();
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card shadow={0} style={{ margin: 'auto' }}>
+        <CardTitle expand style={{ alignItems: 'flex-start' }}>
+          {resource.loginTitle}
+        </CardTitle>
+        <CardText>
+          <Textfield label={resource.fieldLabelAccount} floatingLabel ref={accountRef} />
+          <Textfield label={resource.fieldLabelPassword} floatingLabel ref={passwordRef} type="password" />
+          {error && <p>{resource.badRequest}</p>}
+        </CardText>
+        <CardActions
+          border
+          style={{
+            borderColor: 'rgba(255, 255, 255, 0.2)',
+            display: 'flex',
+            boxSizing: 'border-box',
+            alignItems: 'center'
+          }}
+        >
+          <Button raised colored ripple onClick={handleSubmit}>
+            {resource.login}
+          </Button>
+          <Spacer />
+          <Icon name="account_box" />
+        </CardActions>
+      </Card>
+    </form>
+  );
 }
